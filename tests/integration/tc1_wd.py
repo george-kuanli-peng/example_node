d0b758f11f5ac73fb2719e7310d9455e75084a7e
George Peng <George.Peng@itri.org.tw>
2016-02-17 15:27:32 +0800

    Update selenium test scripts

--- tests/integration/tc1_wd.py
+++ tests/integration/tc1_wd.py
@@ -5,9 +5,11 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
 import unittest, time, re
 
-class Tc1(unittest.TestCase):
+class TC1(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)  # add manually
@@ -24,7 +26,62 @@ class Tc1(unittest.TestCase):
         driver.find_element_by_name("last_name").clear()
         driver.find_element_by_name("last_name").send_keys("Peng")
         driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+        element = WebDriverWait(driver, 10).until(
+                expected_conditions.text_to_be_present_in_element_value((By.CSS_SELECTOR, 'body'), 'First name')
+                )
+
+    def test_tc2(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_name("year").clear()
+        driver.find_element_by_name("year").send_keys("2016")
+        driver.find_element_by_name("month").clear()
+        driver.find_element_by_name("month").send_keys("2")
+        driver.find_element_by_name("day").clear()
+        driver.find_element_by_name("day").send_keys("16")
     
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+class TC2(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)  # add manually
+        # self.base_url = "http://127.0.0.1:1337/"
+        self.base_url = "http://127.0.0.1:1337"  # change manually
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_tc1(self):
+        driver = self.driver
+        driver.get(self.base_url + "/")
+        driver.find_element_by_name("first_name").clear()
+        driver.find_element_by_name("first_name").send_keys("Goerge")
+        driver.find_element_by_name("last_name").clear()
+        driver.find_element_by_name("last_name").send_keys("Peng")
+
     def is_element_present(self, how, what):
         try: self.driver.find_element(by=how, value=what)
         except NoSuchElementException as e: return False
